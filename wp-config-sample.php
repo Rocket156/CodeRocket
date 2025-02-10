0925d501eacdb8c3c53da16ef4a6371884aa5b6b
Rocket156 <149260539+Rocket156@users.noreply.github.com>
2025-02-10 13:22:15 -0600

    Update and rename wp-config.php to wp-config-sample.php

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -20,16 +20,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'u334513510_coderocket' );
+define( 'DB_NAME', '' );
 
 /** Database username */
-define( 'DB_USER', 'u334513510_rocket' );
+define( 'DB_USER', '' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'fPf:HiA!1' );
+define( 'DB_PASSWORD', '' );
 
 /** Database hostname */
-define( 'DB_HOST', '127.0.0.1' );
+define( 'DB_HOST', '' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
